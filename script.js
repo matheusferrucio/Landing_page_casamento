2e6ca588458ced51b7106dd0687699d8b446498d
Matheus Ferrucio <matheusferrucio29@gmail.com>
2025-10-15 10:37:04 -0300

    primeiro commit, inserindo todo código fonte

---
+++ script.js
@@ -0,0 +1,24 @@
+// executa o código só depois que a página carregou por completo
+document.addEventListener('DOMContentLoaded', function(){
+    const btnCopiar = document.getElementById('btnCopiarPix');
+
+    btnCopiar.addEventListener('click', function(){
+        // pega a chave pix do atributo criado 'data-chave'
+        const chavePix = btnCopiar.getAttribute('data-chave');
+
+        // usa a API clipboard para copiar o texto
+        navigator.clipboard.writeText(chavePix).then(()=>{
+
+            // para dar um feedback visual de que a chave foi copiada
+            const textoOriginal = btnCopiar.textContent;
+            btnCopiar.textContent = "Chave copiada";
+
+            setTimeout(() => {
+                btnCopiar.textContent = textoOriginal;
+            }, 3000);
+        }).catch(err => {
+            console.error("Erro ao copiar a chave PIX: ", err);
+            alert("Não foi possível copiar a chave. Tente novamente ou use o QR Code.")
+        });
+    });
+});
